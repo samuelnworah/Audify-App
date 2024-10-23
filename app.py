f1c5980b0d17403c5f0172710d7f6080e18f24f0
Samuel Nworah <oderanworah@gmail.com>
2024-10-23 19:53:35 -0400

    Update to bind the correct port for Render

--- app.py
+++ app.py
@@ -62,4 +62,5 @@ def top_tracks():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT from environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port)
